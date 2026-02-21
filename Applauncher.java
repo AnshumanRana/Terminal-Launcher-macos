e1b52a6f3fb9b6bfe30a4e5c40b129fdd1f62b0d
Anshuman Rana <anshumanrana1404@gmail.com>
2026-02-22 03:21:51 +0530

    feat: add combo launch commands (morning, androiddev, iosdev) and update help menu

--- Applauncher.java
+++ Applauncher.java
@@ -35,6 +35,33 @@ public class Applauncher {
             case "vscode":
                 launchAbleApp("Visual Studio Code");
                 break;
+            case "android":
+                launchAbleApp("Android Studio");
+                break;
+            case "chrome":
+                launchAbleApp("Google Chrome");
+                break;
+            case "safari":
+                launchAbleApp("Safari");
+                break;
+            case "xcode":
+                launchAbleApp("Xcode");
+                break;
+            case "whatsapp":
+                launchAbleApp("WhatsApp");
+                break;
+
+            // ---- Combo Commands ----
+            case "morning":
+                launchAbleApp("WhatsApp", "Brave Browser", "Visual Studio Code");
+                break;
+            case "androiddev":
+                launchAbleApp("Android Studio", "Brave Browser");
+                break;
+            case "iosdev":
+                launchAbleApp("Xcode", "Brave Browser");
+                break;
+
             case "help":
                 printHelp();
                 break;
@@ -65,12 +92,20 @@ public class Applauncher {
 
     public static void printHelp() {
         System.out.println("Available commands:");
-        System.out.println("code - Launch both VSCode and Brave");
-        System.out.println("brave - Launch Brave browser");
-        System.out.println("vscode - Launch Visual Studio Code");
-        System.out.println("help - Show this help message");
-        System.out.println("exit - Quit the App Launcher");
-
+        System.out.println("  code       - Launch VSCode + Brave");
+        System.out.println("  brave      - Launch Brave browser");
+        System.out.println("  vscode     - Launch Visual Studio Code");
+        System.out.println("  android    - Launch Android Studio");
+        System.out.println("  chrome     - Launch Google Chrome");
+        System.out.println("  safari     - Launch Safari");
+        System.out.println("  xcode      - Launch Xcode");
+        System.out.println("  whatsapp   - Launch WhatsApp");
+        System.out.println("  --- Combo Commands ---");
+        System.out.println("  morning    - Launch WhatsApp + Brave + VSCode");
+        System.out.println("  androiddev - Launch Android Studio + Brave");
+        System.out.println("  iosdev     - Launch Xcode + Brave");
+        System.out.println("  help       - Show this help message");
+        System.out.println("  exit       - Quit the App Launcher");
     }
 
 }
